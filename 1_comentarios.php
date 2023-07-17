7c463d4f783692493f7d6ed9e5a743bbddbc045c
omartorres28 <omartorres@colegioguanenta.edu.co>
2023-07-17 17:32:25 -0500

    primer push

---
+++ 1_comentarios.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Comentarioos en php</title>
+</head>
+<body>
+    <h1>Comentarios en PHP</h1>
+    <!--Formas de incluir codigo php -->
+    <?php
+        //Comentario de una linea
+        /*Esto es un comentario de varias lineas*/
+    ?>
+
+    <%
+        codigo php
+    %>
+    
+    <script leguage="php>
+
+    </scrit-->
+</body>
+</html>
